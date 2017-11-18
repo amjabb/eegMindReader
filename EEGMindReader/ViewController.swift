64ce6685dbc8f64414e107becfee0794664f4100
Amir Jabbari <ajabbari2647@yahoo.com>
2017-11-18 15:36:39 -0800

    Initial Commit

---
+++ EEGMindReader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EEGMindReader
+//
+//  Created by Amir Jabbari on 11/18/17.
+//  Copyright © 2017 Amir Jabbari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
