1d8d543edcc3a1ec76d066b15ca032a34c16ac54
Amir Jabbari <ajabbari2647@yahoo.com>
2017-11-26 23:20:24 -0800

    Initial Commit

--- EEGMindReader/AppDelegate.swift
+++ EEGMindReader/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseDatabase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
@@ -27,6 +30,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        let updateValues = ["appDone":1]
+        Database.database().reference().updateChildValues(updateValues)
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
@@ -39,6 +44,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        
     }
 
 
--- EEGMindReader/ViewController.swift
+++ EEGMindReader/ViewController.swift
@@ -7,11 +7,32 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
 class ViewController: UIViewController {
 
+    var ref: DatabaseReference!
+    @IBOutlet weak var testLabel: UILabel!
+    @IBOutlet weak var trainButton: UIButton!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBAction func sendTrainSignal(_ sender: Any) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        titleLabel.text = "WELCOME TO\n THE MOST\n AMAZING\n MACHINE\n LEARNING\n MIND\n CONTROL APP"
+        
+        //Round button
+        trainButton.layer.cornerRadius = 0.5 * trainButton.bounds.size.width
+        trainButton.clipsToBounds = true
+        
+        //Access firebase
+        ref = Database.database().reference()
+        let initValues = ["letters":["A":0, "B":1], "training": ["trainingStart": 0, "trainingDone": 0, "trainingOne": 0, "trainingTwo": 0],  "theme": "Letters", "appStart": 1, "appDone": 0] as [String : Any]
+        ref.updateChildValues(initValues)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
